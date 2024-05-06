81f19662c9cc9232366e9b5a9f94fefda7652921
ajitrhegde400 <46869696+ajitrhegde400@users.noreply.github.com>
2024-05-06 16:30:29 +0530

    Update Application.java

--- src/main/java/com/techshard/graphql/Application.java
+++ src/main/java/com/techshard/graphql/Application.java
@@ -1,4 +1,4 @@
-package com.techshard.graphql;
+erpackage com.techshard.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        System.out.println("Starting Food Delivery Service application");
+        System.out.println("Starting Food Delivery Service- server side application");
         SpringApplication.run(Application.class, args);
     }
 }
